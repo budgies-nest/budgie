4600f45a6621f794499234ce4356c1a274fc7fef
k33g <ph.charriere@gmail.com>
2025-07-06 05:24:22 +0200

    🛟 Updated.

--- cookbook/17-fantastic-creature /chat.sh
+++ cookbook/17-fantastic-creature /chat.sh
@@ -6,3 +6,10 @@ curl --no-buffer http://localhost:5050/api/chat-stream \
 {
   "user": "what is your name?"
 }' 
+
+curl --no-buffer http://localhost:5050/api/chat-stream \
+-H "Content-Type: application/json" \
+-d '
+{
+  "user": "do you know me?"
+}' 
--- cookbook/17-fantastic-creature /main.go
+++ cookbook/17-fantastic-creature /main.go
@@ -13,25 +13,6 @@ import (
 	"github.com/openai/openai-go"
 )
 
-/*
-
-curl http://localhost:12434/engines/llama.cpp/v1/chat/completions \
--H "Content-Type: application/json" \
--d '{
-	"model": "ai/qwen2.5:latest",
-	"messages": [
-		{
-			"role": "system",
-			"content": "You are a helpful assistant."
-		},
-		{
-			"role": "user",
-			"content": "Please write 500 words about the fall of Rome."
-		}
-	],
-	"stream": true
-}'
-*/
 
 func getModelRunnerBaseUrl() string {
 	// Detect if running in a container or locally
@@ -41,23 +22,31 @@ func getModelRunnerBaseUrl() string {
 	return base.DockerModelRunnerContainerURL
 }
 
+// TODO:
+/*
+add the necessary fields to handle the health, strength, ...
+*/
+
 func main() {
 
 	modelRunnerBaseUrl := getModelRunnerBaseUrl()
+	chatAgentName := "Werewolf"
+	chatAgentSystemInstructions := `
+	You are a werewolf
+	Your name is Smity
+	`
 
 	// Enable global logging at Info level
 	//agents.EnableLogging(agents.LogLevelInfo)
 
-	bob, err := agents.NewAgent("Bob",
+	chatAgent, err := agents.NewAgent(chatAgentName,
 		agents.WithDMR(modelRunnerBaseUrl),
 		agents.WithParams(openai.ChatCompletionNewParams{
-			//Model:       "k33g/qwen2.5:0.5b-instruct-q8_0",
 			Model: "hf.co/unsloth/qwen3-0.6b-gguf:q4_k_m",
-			//Model: "unsloth/qwen3-gguf:4B-UD-Q4_K_XL",
 			Temperature: openai.Opt(0.8),
 			Messages: []openai.ChatCompletionMessageParamUnion{
-				openai.SystemMessage("You're a helpful assistant expert with Star Trek universe."),
-				openai.UserMessage("Who is James T Kirk?"),
+				openai.SystemMessage(chatAgentSystemInstructions),
+				//openai.UserMessage("Who is James T Kirk?"),
 			},
 		}),
 		agents.WithLoggingEnabled(),
@@ -65,7 +54,24 @@ func main() {
 		agents.WithHTTPServer(agents.HTTPServerConfig{
 			Port: "5050",
 		}),
+		agents.WithBeforeChatCompletionStream(func(ctx *agents.ChatCompletionStreamContext) {
+			// TODO: make the tool detection here
+			messages := ctx.Agent.Params.Messages
+			// QUESTION: how to get the user message?
+			for idx, message := range messages {
+				content, _ := message.MarshalJSON()
+				
+				fmt.Println("🔴", idx, string(content))
+			}
+		}),
+		agents.WithBeforeChatCompletionStream(func(ctx *agents.ChatCompletionStreamContext) {
+			fmt.Println("👋 Hello World 🌎")
+		}),
 	)
+
+	// QUESTION: how to handle the conversational memory?
+	// NOTE: -> WithAfterCompletionStream (and Before) -> handle an array of messages
+
 	if err != nil {
 		panic(err)
 	}
@@ -75,7 +81,7 @@ func main() {
 	// 	return nil
 	// })
 
-	bob.HttpServer().HandleFunc("POST /api/fight", func(response http.ResponseWriter, request *http.Request) {
+	chatAgent.HttpServer().HandleFunc("POST /api/fight", func(response http.ResponseWriter, request *http.Request) {
 		body := agents.GetBytesBody(request)
 		// unmarshal the json data
 		var data map[string]string
@@ -92,7 +98,7 @@ func main() {
 
 	// Start the HTTP server
 	fmt.Println("Starting HTTP server on port 5050...")
-	err = bob.StartHttpServer()
+	err = chatAgent.StartHttpServer()
 
 	if err != nil {
 		panic(err)
