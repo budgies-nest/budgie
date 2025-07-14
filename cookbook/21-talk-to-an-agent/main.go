06ca3ad4d4977ebac2d0caa00a2a2c28dea977f2
k33g <ph.charriere@gmail.com>
2025-07-14 08:16:01 +0200

    💾 Saved.

---
+++ cookbook/21-talk-to-an-agent/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/budgies-nest/budgie/agents"
+	"github.com/budgies-nest/budgie/enums/base"
+	"github.com/budgies-nest/budgie/enums/environments"
+	"github.com/budgies-nest/budgie/helpers"
+	"github.com/google/uuid"
+	"github.com/openai/openai-go"
+)
+
+func main() {
+	modelRunnerBaseUrl := getModelRunnerBaseUrl()
+
+	sam, err := agents.NewAgent("Bob",
+		agents.WithDMR(modelRunnerBaseUrl),
+		agents.WithParams(
+			openai.ChatCompletionNewParams{
+				//Model:       "ai/qwen2.5:latest",
+				// REF: https://huggingface.co/bartowski/SmolLM2-135M-Instruct-GGUF
+				Model:       "hf.co/bartowski/smollm2-135m-instruct-gguf:q4_k_m",
+				Temperature: openai.Opt(0.0),
+				Messages:    []openai.ChatCompletionMessageParamUnion{},
+			},
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	agentBaseURL := "http://0.0.0.0:8888"
+
+	agentCard, err := sam.PingAgent(agentBaseURL)
+	if err != nil {
+		fmt.Println("Error pinging agent:", err)
+		return
+	}
+	jsonAgentCard, err := agents.AgentCardToJSONString(agentCard)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("🤖 Agent Card:\n", jsonAgentCard)
+
+	taskRequest := agents.TaskRequest{
+		ID:     uuid.NewString(),
+		Method: "message/send",
+		Params: agents.AgentMessageParams{
+			Message: agents.AgentMessage{
+				Role: "user",
+				Parts: []agents.TextPart{
+					{
+						Text: "What is the best pizza in the world?",
+					},
+				},
+			},
+			MetaData: map[string]any{
+				"skill": "ask_for_something",
+			},
+		},
+	}
+	taskResponse, err := sam.SendToAgent(agentBaseURL, taskRequest)
+	if err != nil {
+		fmt.Println("Error sending task request:", err)
+		return
+	}
+
+	jsonTaskResponse, err := agents.TaskResponseToJSONString(taskResponse)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("🟢 Task Response JSON:\n", jsonTaskResponse)
+
+	fmt.Println("🟣 Task Response Text:", taskResponse.Result.History[0].Parts[0].Text)
+
+}
+
+func getModelRunnerBaseUrl() string {
+	// Detect if running in a container or locally
+	if helpers.DetectContainerEnvironment() == environments.Local {
+		return base.DockerModelRunnerLocalURL
+	}
+	return base.DockerModelRunnerContainerURL
+}
---
+++ cookbook/21-talk-to-an-agent/start-server-agent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+: <<'COMMENT'
+
+COMMENT
+
+go run ../20-agent-as-a2a-server/main.go
